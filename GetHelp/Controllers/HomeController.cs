deb04b5ae8b20e1ecc2e4a80a3e1bbf45e095234
Siarhei <6696166@gmail.com>
2016-01-24 19:01:19 +0300

    Add all 2 level references

--- GetHelp/Controllers/HomeController.cs
+++ GetHelp/Controllers/HomeController.cs
@@ -8,37 +8,82 @@ namespace GetHelp.Controllers
 {
     public class HomeController : Controller
     {
+        // Компьютерная помощь
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "Компьютерная помощь";
+            ViewBag.Description = "";
+            ViewBag.KeyWords = "";
 
             return View();
         }
 
+        // Удаление вирусов
         public ActionResult Viruses()
         {
-            ViewBag.Title = "Viruses";
+            ViewBag.Title = "Удаление вирусов";
+            ViewBag.Description = "";
+            ViewBag.KeyWords = "";
 
             return View();
         }
 
+        // Установка Windiws
         public ActionResult InstWin()
         {
-            ViewBag.Title = "Viruses";
+            ViewBag.Title = "Установка Windows";
+            ViewBag.Description = "";
+            ViewBag.KeyWords = "";
 
             return View();
         }
 
+        // Настройка Wi-Fi
         public ActionResult WiFi()
         {
-            ViewBag.Title = "WiFI";
+            ViewBag.Title = "Настройка Wi-Fi";
+            ViewBag.Description = "";
+            ViewBag.KeyWords = "";
 
             return View();
         }
 
+        // Ремонт компьютера
         public ActionResult RepairComp()
         {
-            ViewBag.Title = "RepairComp";
+            ViewBag.Title = "Ремонт компьютеров";
+            ViewBag.Description = "";
+            ViewBag.KeyWords = "";
+
+            return View();
+        }
+
+        // Ремонт ноутбуков
+        public ActionResult RepairBook()
+        {
+            ViewBag.Title = "Ремонт ноутбуков";
+            ViewBag.Description = "";
+            ViewBag.KeyWords = "";
+
+            return View();
+        }
+
+        // Установка программ
+        public ActionResult Programs()
+        {
+            ViewBag.Title = "Установка программ";
+            ViewBag.Description = "";
+            ViewBag.KeyWords = "";
+
+            return View();
+        }
+
+        // Модернизация компьютера
+        public ActionResult Modern()
+        {
+            ViewBag.Title = "Модернизация компьютера";
+            ViewBag.Description = "";
+            ViewBag.KeyWords = "";
 
             return View();
         }
