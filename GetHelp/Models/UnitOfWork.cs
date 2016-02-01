8b08b5e5798711f1e71dd899b955c3c8564a1f5f
Siarhei Strelchonok <6696166@gmail.com>
2016-02-01 13:26:29 +0300

    Add Data base, add iRepository, Add Unit of work

---
+++ GetHelp/Models/UnitOfWork.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GetHelp.Models
+{
+    public class UnitOfWork
+    {
+        private DataBase db = new DataBase();
+        private QueriesRepository queriesRepository;
+    
+        public QueriesRepository Queries
+        {
+            get
+            {
+                if (queriesRepository == null)
+                    queriesRepository = new QueriesRepository(db);
+                return queriesRepository;
+            }
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ GetHelp/Models/iRepo.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using GetHelp.Models;
+using System.Data.Entity;
+
+interface IRepository<T> where T : class
+    {
+    IEnumerable<T> GetAll();
+    void Create(T item);
+    void Update(T item);
+    void Delete(int id);
+    }
+
+public class QueriesRepository : IRepository<QUERIES>
+{
+    private DataBase db;
+
+    public QueriesRepository(DataBase context)
+    {
+        this.db = context;
+    }
+
+    public IEnumerable<QUERIES> GetAll()
+    {
+        return db.QUERIES;
+    }
+
+    public QUERIES Get(int id)
+    {
+        return db.QUERIES.Find(id);
+    }
+
+    public void Create(QUERIES quaries)
+    {
+        db.QUERIES.Add(quaries);
+    }
+
+    public void Update(QUERIES quaries)
+    {
+        db.Entry(quaries).State = EntityState.Modified;
+    }
+
+    public void Delete(int id)
+    {
+
+        QUERIES queries = db.QUERIES.Find(id);
+        if (queries != null) db.QUERIES.Remove(queries);
+    }
+}
+
