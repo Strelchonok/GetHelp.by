3a6d79406cf5077b5664fbebbc9a22995711132c
Siarhei Strelchonok <6696166@gmail.com>
2016-02-03 00:21:46 +0300

    Add angularJs form

---
+++ GetHelp/Controllers/FormController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GetHelp.Models;
+using System.Web.Http.Description;
+
+namespace GetHelp.Controllers
+{
+    public class FormController : ApiController
+    {
+        UnitOfWork unitOfWork;
+        public FormController()
+        {
+            unitOfWork = new UnitOfWork();
+        }
+
+        // POST: api/Form
+        public IHttpActionResult PostFruct(QUERIES queries)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            unitOfWork.Queries.Create(queries);
+            unitOfWork.Save();
+
+            return Ok(queries);
+        }
+    }
+    
+
+}
